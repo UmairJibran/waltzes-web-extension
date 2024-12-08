4566549d367b98efb75b6df8f84dce5750552896
Umair Jibran <me@umairjibran.com>
2024-12-08 13:06:17 +0500

    36-migrate to sidepanel instead of popup (#38)
    * introduce side bar/side panel
    
    * add support for custom jd in sidebar
    
    * clear up the screen from prev elemtns
    
    * language changes

--- src/main.js
+++ src/main.js
@@ -15,8 +15,9 @@ function copyToClipboard(text) {
   document.body.removeChild(copyText);
 
   const div = document.createElement("div");
+  div.id = "copied";
   div.innerHTML = `<div class="mt-2">
-    <small><code>Coppied to your clipboard</code></small>
+    <small><code>Copied to your clipboard</code></small>
   </div>`;
 
   const mainDiv = document.getElementById("popup");
@@ -25,6 +26,7 @@ function copyToClipboard(text) {
 
 function notSupported(title, url) {
   const div = document.createElement("div");
+  div.id = "not-supported";
   div.innerHTML = `<div class="mt-2">
     <p><code>${title}</code> is not <em>yet</em> supported</p>
     <a href="https://github.com/UmairJibran/waltzes/issues/new?title=${title}&body=${url}" target="_blank">Request support</a>
@@ -35,6 +37,7 @@ function notSupported(title, url) {
 
 function acknowledgeUser(bestMatchSection) {
   const div = document.createElement("div");
+  div.id = "acknowledgement";
   div.innerHTML = `<div class="mt-2">
     <p>Cover Letter created based on: <span class="font-monospace">${bestMatchSection}</span></p>
   </div>`;
@@ -63,34 +66,59 @@ function showCoverLetter(coverLetter) {
 
   const mainDiv = document.getElementById("popup");
   mainDiv.appendChild(copyButton);
+  copyButton.id = "copy-button";
   mainDiv.appendChild(div);
+  div.id = "cover-letter";
   mainDiv.appendChild(clArea);
+  clArea.id = "cl-area";
 }
 
 generateCLButton.addEventListener("click", async function () {
+  [
+    "not-supported",
+    "acknowledgement",
+    "cover-letter",
+    "copy-button",
+    "cl-area",
+    "copied",
+  ].forEach((id) => {
+    const element = document.getElementById(id);
+    if (element) element.remove();
+  });
+
   const loader = document.getElementById("loader");
-  const currentTab = await getCurrentTab();
+  const customJd = document.getElementById("custom-job-description");
   const savedHost = localStorage.getItem("host") || "http://localhost:5000";
   const baseUrl = [savedHost, "job-details"].join("/");
-  let jobBoard = "";
-  if (currentTab.url.includes("greenhouse")) jobBoard = "greenhouse";
-  if (currentTab.url.includes("lever")) jobBoard = "lever";
+  let url = "";
+  if (customJd && customJd.value) {
+    url = `${baseUrl}/custom-jd`;
+  } else {
+    const currentTab = await getCurrentTab();
+    let jobBoard = "";
+    if (currentTab.url.includes("greenhouse")) jobBoard = "greenhouse";
+    if (currentTab.url.includes("lever")) jobBoard = "lever";
 
-  if (!jobBoard) {
-    notSupported(currentTab.title, currentTab.url);
-    return;
+    if (!jobBoard) {
+      notSupported(currentTab.title, currentTab.url);
+      return;
+    }
+    url = `${baseUrl}/${jobBoard}?url=${currentTab.url}`;
   }
+
   const openAiKey = localStorage.getItem("openAiKey");
-  let url = `${baseUrl}/${jobBoard}?url=${currentTab.url}`;
   if (openAiKey) url += `&openAiKey=${openAiKey}`;
 
   generateCLButton.disabled = true;
   loader.hidden = false;
   const response = await fetch(url, {
-    method: "GET",
+    method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
+    body: JSON.stringify({
+      ...(customJd && { customJd: customJd.value }),
+    }),
     mode: "cors",
   });
 
